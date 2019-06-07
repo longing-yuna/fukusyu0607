04fbaff468b51b160379f940a228dfc3929f3aa3
pizza_girl <pizza.girl.dat@gmail.com>
2019-06-07 13:52:32 +0900

    １回目

--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -12,9 +12,44 @@ namespace fukusyu0607
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Text = MousePosition.X + "," + MousePosition.Y;
+            Point p = PointToClient(MousePosition);
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+            if (label1.Left > ClientSize.Width - label1.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Top > ClientSize.Height - label1.Height)
+            {
+                vy = -vy;
+            }
+            if ((p.X >= label1.Left) && (p.X <= label1.Right) && (p.Y >= label1.Top) && (p.Y <= label1.Bottom))
+                timer1.Enabled = false;
+        }
+       
     }
 }
