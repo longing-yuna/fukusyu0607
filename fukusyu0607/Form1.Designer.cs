e92dcf53e49efd5ec26f68bf7f0eb511b0a0b9c9
pizza_girl <pizza.girl.dat@gmail.com>
2019-06-07 16:01:27 +0900

    kairyo

--- fukusyu0607/Form1.Designer.cs
+++ fukusyu0607/Form1.Designer.cs
@@ -31,16 +31,19 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 52);
+            this.label1.Location = new System.Drawing.Point(62, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 12);
+            this.label1.Size = new System.Drawing.Size(52, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "´・ω・`)≡（`・ω・´）≡(゜∀゜";
+            this.label1.Text = "（`・ω・´）";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
@@ -48,11 +51,44 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "（`・ω・´）";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "（`・ω・´）";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Timer:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -65,6 +101,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -12,11 +12,36 @@ namespace fukusyu0607
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        //議事乱数
+        //ランダムのシード（種）を指定して初期化したら
+        //それを使い続ける
+
+
+        private static Random rand = new Random();
+        
+        int left = 3;
+        int time = 100 * 5;
+        int[] velx = new int[3];
+        int[] vely = new int[3];
+
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 3; i++ )
+            {
+                velx[i] = rand.Next(-10, 11);
+                vely[i] = rand.Next(-10, 11);
+            }
+               
+            //以下に、label1.Leftとlabel1.Topの座標をランダムで求めよ
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Height - label1.Height);
+            label2.Left = rand.Next(ClientSize.Width - label2.Width);
+            label2.Top = rand.Next(ClientSize.Height - label2.Height);
+            label3.Left = rand.Next(ClientSize.Width - label3.Width);
+            label3.Top = rand.Next(ClientSize.Height - label3.Height);
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -26,30 +51,95 @@ namespace fukusyu0607
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            label1.Left += velx[0];
+            label1.Top += velx[1];
+            label2.Left += velx[2];
+            label2.Top += vely[0];
+            label3.Left += vely[1];
+            label3.Top += vely[2];
+          
             Text = MousePosition.X + "," + MousePosition.Y;
             Point p = PointToClient(MousePosition);
-            label1.Left += vx;
-            label1.Top += vy;
-
+            /* if(   (p.X>=label1.Left)
+                 &&(p.X<label1.Right)
+                 &&(p.Y>=label1.Top)
+                 &&(p.Y<label1.Bottom)
+                 )
+              */
             if (label1.Left < 0)
             {
-                vx = Math.Abs(vx);
+                velx[0] = Math.Abs(velx[0]);
             }
             if (label1.Top < 0)
             {
-                vy = -vy;
+                velx[1] = -velx[1];
             }
             if (label1.Left > ClientSize.Width - label1.Width)
             {
-                vx = -Math.Abs(vx);
+                velx[0] = -Math.Abs(velx[0]);
             }
             if (label1.Top > ClientSize.Height - label1.Height)
             {
-                vy = -vy;
+                velx[1] = -velx[1];
             }
-            if ((p.X >= label1.Left) && (p.X <= label1.Right) && (p.Y >= label1.Top) && (p.Y <= label1.Bottom))
+            if ((p.X >= label1.Left) && (p.X <= label1.Right) && (p.Y >= label1.Top) && (p.Y <= label1.Bottom)&& label1.Visible)
                 timer1.Enabled = false;
+
+            if (label2.Left < 0)
+            {
+                velx[2] = Math.Abs(velx[2]);
+            }
+            if (label2.Top < 0)
+            {
+                vely[0] = -vely[0];
+            }
+            if (label2.Left > ClientSize.Width - label2.Width)
+            {
+                velx[2] = -Math.Abs(velx[2]);
+            }
+            if (label2.Top > ClientSize.Height - label2.Height)
+            {
+                vely[0] = -vely[0];
+                if ((p.X >= label2.Left) && (p.X <= label2.Right) && (p.Y >= label2.Top) && (p.Y <= label2.Bottom) && label2.Visible)
+                    timer1.Enabled = false;
+            }
+            if (label3.Left < 0)
+            {
+                vely[1] = Math.Abs(vely[1]);
+            }
+            if (label3.Top < 0)
+            {
+                vely[2] = -vely[2];
+            }
+            if (label3.Left > ClientSize.Width - label3.Width)
+            {
+                vely[1] = -Math.Abs(vely[1]);
+            }
+            if (label3.Top > ClientSize.Height - label3.Height)
+            {
+                vely[2] = -vely[2];
+            }
+            if ((p.X >= label3.Left) && (p.X <= label3.Right) && (p.Y >= label3.Top) && (p.Y <= label3.Bottom) && label3.Visible)
+                timer1.Enabled = false;
+            time--;
+            label5.Text = "Time: " + time;
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+}
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+       
        
     }
 }
